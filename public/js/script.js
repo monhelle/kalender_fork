0eaa37c24a23d80ea09c5b88db3e3f9062f58e77
Nyan <nyandude22@gmail.com>
2023-11-21 10:33:41 +0100

    Added gift style

--- public/js/script.js
+++ public/js/script.js
@@ -12,16 +12,35 @@ document.addEventListener('DOMContentLoaded', function() {
         gridItem.className = 'grid-item';
         gridItem.textContent = i;
 
+        // Additional elements for the gift box design
+        var ribbon = document.createElement('div');
+        ribbon.className = 'ribbon';
+        gridItem.appendChild(ribbon);
+
+        var bow = document.createElement('div');
+        bow.className = 'bow';
+        gridItem.appendChild(bow);
+
+        var lid = document.createElement('div');
+        lid.className = 'lid';
+        gridItem.appendChild(lid);
+
         // Check if the item can be opened based on the day in December
         if (i <= currentDayInDecember) {
             gridItem.classList.add('openable');
+            ribbon.classList.add('openable-rib');
+            bow.classList.add('openable-rib');
+            lid.classList.add('openable-lid');
             gridItem.addEventListener('click', function() {
                 // Add your custom logic for handling opened items
                 var itemNumber = this.textContent;
-                window.location.href = './december/' + itemNumber + '/content.html'// Redirect to a new page with the item number as a parameter
+                window.location.href = './december/' + itemNumber + '/content.html'; // Redirect to a new page with the item number as a parameter
             });
         } else {
             gridItem.classList.add('unopenable');
+            ribbon.classList.add('unopenable-rib');
+            bow.classList.add('unopenable-rib');
+            lid.classList.add('unopenable-lid');
         }
 
         gridContainer.appendChild(gridItem);
