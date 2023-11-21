d42dc370316ef58a710347598e249d54c502fc8e
Nyan <nyandude22@gmail.com>
2023-11-21 11:57:34 +0100

    added final css

--- public/js/script.js
+++ public/js/script.js
@@ -10,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
     for (var i = 1; i <= 24; i++) {
         var gridItem = document.createElement('div');
         gridItem.className = 'grid-item';
-        gridItem.textContent = i;
 
         // Additional elements for the gift box design
         var ribbon = document.createElement('div');
@@ -25,15 +24,30 @@ document.addEventListener('DOMContentLoaded', function() {
         lid.className = 'lid';
         gridItem.appendChild(lid);
 
+        // Create a background element
+        var background = document.createElement('div');
+        background.className = 'background';
+        gridItem.appendChild(background);
+
+        // Create the text content element
+        var textContent = document.createElement('i');
+        textContent.textContent = i;
+        textContent.className = 'grid-item-text'; // Add a specific class for styling
+
+        // Append the text content to the grid item
+        gridItem.appendChild(textContent);
+
         // Check if the item can be opened based on the day in December
         if (i <= currentDayInDecember) {
             gridItem.classList.add('openable');
             ribbon.classList.add('openable-rib');
             bow.classList.add('openable-rib');
             lid.classList.add('openable-lid');
+            background.classList.add('openable-bg'); // Add a class for openable background styling
+            textContent.classList.add('openable-text'); // Add a class for openable text styling
             gridItem.addEventListener('click', function() {
                 // Add your custom logic for handling opened items
-                var itemNumber = this.textContent;
+                var itemNumber = this.querySelector('.grid-item-text').textContent;
                 window.location.href = './december/' + itemNumber + '/content.html'; // Redirect to a new page with the item number as a parameter
             });
         } else {
@@ -41,6 +55,8 @@ document.addEventListener('DOMContentLoaded', function() {
             ribbon.classList.add('unopenable-rib');
             bow.classList.add('unopenable-rib');
             lid.classList.add('unopenable-lid');
+            background.classList.add('unopenable-bg'); // Add a class for unopenable background styling
+            textContent.classList.add('unopenable-text'); // Add a class for unopenable text styling
         }
 
         gridContainer.appendChild(gridItem);
