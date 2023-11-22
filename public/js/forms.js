82359be86cef7be3675626de995abbbf2eb1d11d
mleitejens <mleitejensen@gmail.com>
2023-11-22 10:02:05 +0100

    Added clicking on the sides of the date pages to go back and forth

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,4 +1,5 @@
 let form = document.querySelector(".form")
+const body = document.querySelector("body")
 
 html = `<form>
 <label for="name">Skriv navnet ditt her</label><br>
@@ -9,3 +10,54 @@ html = `<form>
 </form>`
 
 form.innerHTML = html
+
+body.innerHTML += `
+<div class="clickLeft"></div>
+<div class="clickRight"></div>
+`
+
+const left = document.querySelector(".clickLeft")
+const right = document.querySelector(".clickRight")
+
+let currentDate = new Date()
+let currentUrl = window.location.pathname.split('/')
+
+if(currentUrl[2] == 1){
+    console.log("start")
+    left.addEventListener("click", () => {
+        window.location = `/`
+    })
+    right.addEventListener("click", () => {
+        let date = Number(currentUrl[2]) 
+        window.location = `/december/${date + 1}`
+    })
+
+} else if (currentUrl[2] == currentDate.getDate()){
+    left.addEventListener("click", () => {
+        let date = Number(currentUrl[2])
+        window.location = `/december/${date - 1}`
+    })
+    right.addEventListener("click", () => {
+        window.alert("Det er ikke den datoen ennå")
+    })
+
+}else if(currentUrl[2] == 24){
+    left.addEventListener("click", () => {
+        let date = Number(currentUrl[2])
+        window.location = `/december/${date - 1}`
+    })
+    right.addEventListener("click", () => {
+        window.location = `/`
+    })
+
+} else{
+    left.addEventListener("click", () => {
+        let date = Number(currentUrl[2])
+        window.location = `/december/${date - 1}`
+    })
+    right.addEventListener("click", () => {
+        let date = Number(currentUrl[2]) 
+        window.location = `/december/${date + 1}`
+    })
+}
+
