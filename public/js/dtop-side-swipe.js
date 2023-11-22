4daa0f9768c3764d3bdd30ccfe8c81947576d2dd
Nyan <nyandude22@gmail.com>
2023-11-22 08:35:10 +0100

    Added escape key

--- public/js/dtop-side-swipe.js
+++ public/js/dtop-side-swipe.js
@@ -1,30 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
+     console.log('DOM content loaded');
     var currentDayInDecember = new Date().getDate();
 
-    document.addEventListener('keydown', function(e) {
-        var direction = '';
+    document.addEventListener('keyup', function(e) {
+    console.log("Key released:", e.key);
 
-        if (e.keyCode === 37) {
-            // Left arrow key
-            direction = 'left';
-        } else if (e.keyCode === 39) {
-            // Right arrow key
-            direction = 'right';
-        }
+    if (e.key === 'Escape') {
+        window.location.replace('/');
+    }
+    });
+
+
+    document.addEventListener('keydown', function(e) {
+        console.log("Key pressed:", e.key);
+
+        var direction = e.key === 'ArrowLeft' ? 'left' : e.key === 'ArrowRight' ? 'right' : '';
 
         if (direction !== '') {
             var pathParts = window.location.pathname.split('/');
             var itemNumberIndex = pathParts.indexOf('december') + 1;
             var itemNumber = itemNumberIndex < pathParts.length ? parseInt(pathParts[itemNumberIndex]) : NaN;
 
+            console.log("Item Number:", itemNumber);
+
             if (!isNaN(itemNumber)) {
                 var nextItemNumber;
+
 
                 if (direction === 'left' && itemNumber === 1) {
                     // Left arrow at item 1, redirect to root
                     var rootUrl = '/';
                     console.log("Redirecting to root:", rootUrl);
-                    window.location.href = rootUrl;
+                    window.location.assign(rootUrl);
                     return;
                 }
 
@@ -36,21 +43,25 @@ document.addEventListener('DOMContentLoaded', function() {
                     nextItemNumber = itemNumber + 1;
                 }
 
+                console.log("Next Item Number:", nextItemNumber);
+
                 if (nextItemNumber <= currentDayInDecember) {
                     var nextItemUrl = '/' + nextItemNumber + '/';
                     console.log('Next item URL:', nextItemUrl);
 
-                    pathParts.splice(itemNumberIndex, 1);
-                    pathParts.splice(itemNumberIndex, 0, nextItemNumber);
+                    // Update the URL without 'december' part
+                    pathParts[itemNumberIndex] = nextItemNumber;
 
                     var finalUrl = pathParts.join('/');
                     console.log('Final URL:', finalUrl);
 
-                    window.location.href = finalUrl;
+                    // Directly update the pathname
+                    window.location.pathname = finalUrl;
                 } else {
                     alert("Ikke den datoen ennå");
                 }
             }
         }
     });
+
 });
