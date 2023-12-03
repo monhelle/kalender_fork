6ea0841d8cf5fe19c7064fdad5ca9decf4c2083e
monhelle <monhelle@gmail.com>
2023-12-03 21:10:10 +0100

    adding geirs info

--- app.js
+++ app.js
@@ -9,16 +9,17 @@ const PORT = process.env.PORT || 3000;
 const dbname = process.env.DBNAME;
 const dbstring = process.env.DBSTRING;
 
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public/'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, () => {
+app.listen(3000, () => {
     connectToDB(dbstring, dbname);
     console.log(`Server running on port ${PORT}\nopen http://localhost:${PORT} to view the page`);
-    if(process.env.DEBUG) {
+    if(process.env.DEBUG == true) {
         console.log('WARNING! CURRENTLY IN DEBUG MODE!');
     }
 });
